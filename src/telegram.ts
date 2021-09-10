1633bcc873cbdd290a9076ebb185fa25deb51a35
Norton Wang <1502552+FloorLamp@users.noreply.github.com>
2021-09-09 22:35:40 -0400

    Update chat id

--- src/telegram.ts
+++ src/telegram.ts
@@ -4,4 +4,4 @@ export const BOT_TOKEN = "1999811866:AAFPp-5Fiow6p5vb4VuDxaUU1uZVhX1j6jw";
 
 export const bot = new Telegraf(BOT_TOKEN);
 
-export const CHAT_ID = "-1001338717576";
+export const CHAT_ID = "@cubicplace";
